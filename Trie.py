2a19af331c63f03831c177706458df9d9c3e2c51
= <artem.golotin@gmail.com>
2015-10-14 17:01:17 -0600

    Trie matching problem complete

--- Trie.py
+++ Trie.py
@@ -10,6 +10,7 @@ class Trie(object):
         self.nodes = {1:self.create_node(0,0)}
         self.edges = {}
 
+        # I just wanted to make it cool and allow for both a string and a list to be passed into the init argument
         if type(words) is str:
             self.add_word(word)
         else:
@@ -17,8 +18,12 @@ class Trie(object):
                 self.add_word(w)
                 
                 
+
                 
     def add_word(self, word):
+        # get the number of the parent node and the substring to insert
+        # since part of the trie may already contain part of the word we want to insert
+        #    we only want to get the substring that we have to add to the trie
        parent_node, insertion_substr = self.insertion_point(word, 1)
 
        for i in xrange(len(insertion_substr)):
@@ -27,6 +32,8 @@ class Trie(object):
            self.nodes[new_node_key] = self.create_node(parent_node, self.nodes[parent_node]['depth']+1)
            self.nodes[parent_node]['children'].append(new_node_key)
 
+           # Every edge in a dictionary is stroed as a key value pair, where key is a tuple comprised of the 
+           #    parent node number and the node number of the value. Value is the actual nucleotide
            self.edges[parent_node, new_node_key] = insertion_substr[i]
 
            parent_node = new_node_key
@@ -34,6 +41,10 @@ class Trie(object):
        self.nodes[parent_node]['end'] = True
 
 
+
+
+    # Traverses the trie to find where to insert the next new node
+    # Function is used by add_word()
     def insertion_point(self, word_to_add, current_node):
 
         if word_to_add == '':
@@ -46,3 +57,22 @@ class Trie(object):
         return current_node, word_to_add
 
 
+
+    # Indentifies whether a prefix to a dna sequence is in the trie
+    # Function is used only in trie matching problem(problem 26)
+    # @param word is the dna sequence
+    def prefix_in_trie(self, word, current_node):
+
+        if self.nodes[current_node]['end'] == True:
+            # If we reach the end of the trie we know that the suffix is in the trie
+            return True
+        elif word == '':
+            return False;
+
+        # Recursively traverse the trie and propogate the boolean up
+        for child_node in self.nodes[current_node]['children']:
+            if self.edges[current_node, child_node] == word[0]:
+                return self.prefix_in_trie(word[1:], child_node)
+
+
+        return False
--- problm25.py
+++ problm25.py
@@ -4,17 +4,22 @@ import sys
 from Trie import Trie 
 from operator import sub
 
-with open(sys.argv[1]) as fd:
-    seqs = [line.strip() for line in fd.readlines()]
+def trie_construction(seqs):
+    trie = Trie(seqs)
 
-trie = Trie(seqs)
+    output = list()
+    for item in trie.edges.items():
+        output.append("->".join(map(str, map(sub, item[0], (1,1)))) + ":" + item[1])
 
-output = list()
-for item in trie.edges.items():
-    output.append("->".join(map(str, map(sub, item[0], (1,1)))) + ":" + item[1])
+    def getKey(item):
+        return item[3] "1->2:A"
 
-def getKey(item):
-    return item[3] "1->2:A"
+    return "\n".join(sorted(output, key=getKey))
 
-print "\n".join(sorted(output, key=getKey))
+if __name__ == "__main__":
+
+    with open(sys.argv[1]) as fd:
+        seqs = [line.strip() for line in fd.readlines()]
+
+    print trie_construction(seqs)
 
