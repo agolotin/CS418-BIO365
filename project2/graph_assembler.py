6e4443dcd0484e9c034acc75534399ba3bc143f8
= <artem.golotin@gmail.com>
2015-10-20 16:14:00 -0600

    Project 2 error checking working

--- project2/graph_assembler.py
+++ project2/graph_assembler.py
@@ -1,24 +1,42 @@
 #! /usr/bin/env python
 
 import sys
+from collections import defaultdict
+from math import floor
 
 try:
     with open(sys.argv[1]) as fd:
-        _kmers = [seq.strip() for seq in fd.readlines()]
-        kmer_list = filter(lambda a : a[0] != '>', _kmers)
-        k = sys.argv[2]
+        sequences = filter(lambda a : a[0] != '>', [seq.strip() for seq in fd])
+        k = int(sys.argv[2])
 except:
     print "USAGE: python graph_assembler.py <file_path> <kmer size>"
     sys.exit()
 
-print kmer_list
-sys.exit()
+#  ============================== ACCOUNT FOR THE ERROR IN READS ================================ ||
+kmers = defaultdict(int)
+for seq in sequences:
+    for i in xrange(len(seq) - k + 1):
+        kmers[seq[i:i+k]] += 1
+
+max_occurences = max(kmers.values()) # get the maximum number of occurences of a kmer
+
+# we will ignore only 1% of all of the values
+values_to_ignore = [int(floor(float(max_occurences) * 0.01))]
+if values_to_ignore[0] > 1:
+    values_to_ignore = range(1, values_to_ignore[0] + 1)
+# filter the key-value pairs to match only the ones we need 
+kmers = filter(lambda pair: pair[1] not in values_to_ignore, kmers.iteritems())
+# now we are going to create a list of only kmers out of our kmer:num_occurences dictionary of kmers
+kmer_list = list(zip(*kmers)[0])
+
+#  ============================================================================================== ||
 
 graph = dict()
 for kmer in kmer_list:
     if kmer[:-1] not in graph:
         graph[kmer[:-1]] = list()
     graph[kmer[:-1]].append(kmer[1:])
+
 
 balanced, unbalanced = list(), list()
 out_values = reduce(lambda a,b: a+b, graph.values())
@@ -48,7 +66,7 @@ def getContigs(node_first, node_second):
 
 contig_output = list()
 for node in (set(unbalanced) & set(graph.keys())):
-    contig_output += getContigs(node, node)
+    contig_output += sorted(getContigs(node, node))
 
 print " ".join(sorted(contig_output))
 
