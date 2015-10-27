fefbb3e28e09a387343b23a91a72543c2e6b8e35
= <artem.golotin@gmail.com>
2015-10-26 21:24:54 -0600

    The script to run assemblers

--- project2/tests/run_assemblers.py
+++ project2/tests/run_assemblers.py
@@ -4,9 +4,9 @@ import os
 
 noerror_datasets = ['../datasets/synthetic.example.noerror.small.fasta', '../datasets/synthetic.noerror.small.fasta', '../datasets/synthetic.noerror.large.fasta']
 outputs = ['output/synthetic.example.noerror.small.fasta', 'output/synthetic.noerror.small.fasta', 'output/synthetic.noerror.large.fasta', 'output/real.error.small.fasta', 'output/real.error.large.fasta']
-real_datasets = ['real.error.small.fasta', 'real.error.large.fasta']
+real_datasets = ['../datasets/real.error.small.fasta', '../datasets/real.error.large.fasta']
 
-kmer_list = [7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+kmer_list = [11,13,15,17,19,21,23,25,27,29,31]
 
 
 i = 0
@@ -16,11 +16,12 @@ for _file in noerror_datasets:
         os.system("python assembler.py " + _file + " " + str(kmer_size) + " noerror > " + outputs[i] + ".output.kmer_size"+str(kmer_size))
     i += 1
 
-#kmer_size = 10
-#percentages = [0.01, 0.05, 0.1, 0.15]
-#for _file in real_datasets:
-#    for percentage in percentages:
-#        print "python assembler ../" + _file + " " + str(kmer_size) + " noerror > " + _file + ".output.kmer_size"+str(kmer_size)
-#        os.system("python assembler.py ../" + _file + " " + kmer_size " error " + percentage + " > " + _file + ".output.assembler"+percentage)
-#        os.system("python graph_assembler.py ../" + _file + " " + kmer_size " error " + percentage + " > " + _file + ".output.graph_assember"+percentage)
-#    sys.exit()
+k = 0
+kmer_size = [15, 31] # best kmer size found using kmer genie
+percentages = [0.1, 0.15, 0.20, 0.25]
+for _file in real_datasets:
+    for percentage in percentages:
+#        os.system("python assembler.py " + _file + " " + str(kmer_size[k]) + " error " + str(percentage) + " > " + outputs[i] + ".output.assembler.percent"+str(percentage))
+        os.system("python graph_assembler.py " + _file + " " + str(kmer_size[k]) + " error " + str(percentage) + " > " + outputs[i] + ".output.graph_assember.percent"+str(percentage))
+    k += 1
+    i += 1
