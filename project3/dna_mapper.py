0cc4ec0b7aab8ba6718e270e7ce5685a295ea6ff
= <artem.golotin@gmail.com>
2015-11-06 07:18:51 -0700

    Project 3 is at its middle point. Need to implement backtracking and speculative path extending

---
+++ project3/dna_mapper.py
@@ -0,0 +1,173 @@
+#! /usr/bin/env python
+
+from suffix_tree import SuffixTree
+from collections import defaultdict
+from datetime import datetime
+import itertools
+import operator
+import sys
+
+class overlapFinder(object):
+
+    def __init__(self, sa, fr, bwt, ltf):
+        self.suffix_array = sa
+        self.first_rotation = fr
+        self.bw_transform  = bwt
+        self.last_to_first = ltf
+
+
+
+    def findOverlaps(self, read):
+        reverse_read = read[1][::-1]
+
+        lft_indicies = list()
+        previous_bw = self.first_rotation
+
+        for i in xrange(len(read[1])-1):
+            cur_char = reverse_read[i]
+            next_char = reverse_read[i+1]
+
+            first_indicies = [index for index, value in enumerate(self.first_rotation) 
+                    if value[0] == cur_char and value in previous_bw]
+
+            bw_indicies = [index for index in range(first_indicies[0],first_indicies[-1]+1) 
+                    if self.bw_transform[index][0] == next_char]
+            previous_bw = [self.bw_transform[bw_indicies[0]], self.bw_transform[bw_indicies[-1]]]
+
+            lft_indicies = [self.last_to_first[bw_indicies[0]], self.last_to_first[bw_indicies[-1]]]
+
+        print "[Logging " + getTime() + "] Read '" + read[0] + "'+ has been mapped"
+        return list(set([self.suffix_array[i] for i in lft_indicies]))
+
+'''
+ACT
+T: ltf 10, 11
+C: ltf 5, 6
+sa[5] and sa[6] - the positions where text occurs
+
+eq:  A     C     T     G     A    C    A    T    G     A    C     T    G     A    A    C     A    $
+pos: 0     1     2     3     4    5    6    7    8     9    10    11   12    13   14   15    16   17
+sa:  17    16    13    14    4    9    0    6    15    5    10    1    12    3    8    11    2    7
+1st: $_1   A_1   A_2   A_3   A_4  A_5  A_6  A_7  C_1   C_2  C_3   C_4  G_1   G_2  G_3  T_1   T_2  T_3
+BWT: A_1   C_1   G_1   A_2   G_2  G_3  $_1  C_2  A_3   A_4  A_5   A_6  T_1   T_2  T_3  C_3   C_4  A_7
+ltf: 1     8     12    2     13   14   0    9    3     4    5     6    15    16   17   10    11   7
+
+
+
+main_sequence = "ACTGACATGACTGAACA$"
+reads = ["AACA"]
+main_sequence = "AATCGGGTTCAATCGGGGT$"
+reads = ["ATCG", "GGGT"]
+'''
+
+
+
+def constructSuffixArray(main_sequence):
+
+    tree = SuffixTree(len(main_sequence))
+
+    for char in main_sequence:
+        tree.add_char(char)
+#    tree.print_graphviz_tree()
+
+    return tree.depthFirstSearch()
+
+def _enumerate(sequence):
+    enumerated = list()
+    char_count = defaultdict(int)
+
+    for char in sequence:
+        char_count[char] += 1
+        enumerated.append(char + str(char_count[char]))
+
+    return enumerated
+
+def BurrowsWheelerTransform(seq, suffix_array):
+    first_rotation = list()
+    bw_transform = list()
+    last_char_position = len(seq)-1
+
+    for index in suffix_array:
+        first_rotation.append(seq[index])
+        bw_transform.append(seq[ index-1 if index != 0 else last_char_position ])
+
+    return _enumerate(first_rotation), _enumerate(bw_transform)
+
+def constructLTF(first_rotation, bw_transform):
+    last_to_first = [first_rotation.index(key) for key in bw_transform]
+
+    return last_to_first
+
+
+def createSam(reference, read_overlaps):
+    output = ""
+
+    for read_tuple, overlap_indicies in read_overlaps:
+        for overlap_index in overlap_indicies:
+
+            output += str(read_tuple[0]) + "\t0\t" 
+            output += reference + "\t" + str(overlap_index+1)
+            output += "\t" + str(255) + "\t" + str(len(read_tuple[1])) + "M"
+            output += "\t" + "*\t0\t0\t" + read_tuple[1] + "\t*\n"
+
+    return output
+
+def getTime():
+    current_time = datetime.now()
+
+    year = current_time.year
+    month = current_time.month
+    hour = current_time.hour
+    minute = current_time.minute
+    second = current_time.second
+
+    return "{0}/{1} {2}:{3}:{4}".format(year, month, hour, minute, second)
+
+if __name__ == "__main__":
+
+    try: 
+
+        with open(sys.argv[1]) as fd:
+            reference = fd.readline().strip()[1:]
+            main_sequence = "".join([seq.strip().lower() for seq in fd]) + "$"
+        with open(sys.argv[2]) as fd:
+            reads = [(header.strip()[1:], seq.strip().lower()) for header, seq in itertools.izip_longest(*[fd]*2)]
+    except IndexError:
+        print "USAGE: python dna_mapper.py <chromosome_file> <reads_file>"
+        sys.exit()
+
+    try:
+        print "[Logging " + getTime() + "] Files loaded"
+        suffix_array = constructSuffixArray(main_sequence)
+        assert len(suffix_array) == len(main_sequence)
+
+        print "[Logging " + getTime() + "] Suffix array constructed"
+        first_rotation, bw_transform = BurrowsWheelerTransform(main_sequence, suffix_array)
+        assert len(first_rotation) == len(bw_transform) == len(main_sequence)
+
+        print "[Logging " + getTime() + "] The first rotation and the Burrows-Wheeler Transform constructed"
+        last_to_first = constructLTF(first_rotation, bw_transform)
+        assert len(last_to_first) == len(main_sequence)
+
+        print "[Logging " + getTime() + "] Last-to-first array constructed"
+        # Custom data structure that holds all of the other necessary data structures.
+        # Should be written to a file later
+        finder = overlapFinder(suffix_array, first_rotation, bw_transform, last_to_first)
+
+        # Find there overlaps exist in reads
+        print "[bogging " + getTime() + "] Searching for read overlaps"
+        read_overlaps = [(single_read, finder.findOverlaps(single_read)) for single_read in reads]
+
+        print "[Logging " + getTime() + "] Read overlap map has been constructed"
+        sam_output = createSam(reference, read_overlaps)
+        
+        with open(sys.argv[1] + ".sam", "w+") as fd:
+            fd.write(sam_output)
+        print "[Logging " + getTime() + "] SAM file was written into the working directory as " + sys.argv[1] + ".sam"
+
+    # Error occured when doing assertion
+    except AssertionError:
+        print "[ERROR " + getTime() + "] Check assert statements. Lengths of certain data structures do not match"
+        sys.exit()
+
+
---
+++ project3/suffix_tree.py
@@ -0,0 +1,218 @@
+#! /usr/bin/env python
+
+import sys
+import operator
+
+oo = sys.maxint / 2 # oo is infinity
+
+class Node:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.edges = {}
+        self.link = 0
+
+    def edge_length( self, position ):
+        return min( self.end, position + 1 ) - self.start
+
+    def get_edges( self ):
+        return self.edges
+
+    def set_start( self, start ):
+        self.start = start
+
+    def get_start( self ):
+        return self.start
+
+    def get_end( self ):
+        return self.end
+
+    def set_end( self, end ):
+        self.end = end
+
+    def put_edge( self, key, val ):
+        self.edges[ key ] = val
+
+    def set_link( self, node ):
+        self.link = node
+
+    def get_link( self ):
+        return self.link
+
+    def get_edge( self, key ):
+        if key in self.edges:
+            return self.edges[ key ]
+        else:
+            return None
+
+    def str_val( self ):
+        string = "start: " + str( self.start ) + " end: " + str( self.end ) + " edges: " + str( self.edges )
+        return string
+
+    def increment_start( self, val ):
+        self.start += val
+
+
+class SuffixTree:
+    def __init__( self, t_len ):
+        self.cur_node = 0
+        self.nodes_added = 0
+        self.nodes = [ Node( 0, oo ) ] * ( 2 * t_len + 2 )
+        self.text = ""
+        self.root = self.new_node( -1, -1 )
+        self.pos = -1
+        self.need_link = 0
+        self.r = 0   # remainder - how many suffixes you haven't added in yet
+
+        self.active_node = self.root
+        self.active_edge = 0
+        self.active_length = 0
+
+    '''__author__ = agolotin '''
+    ''' Performs recursive depth first search of the suffix tree  
+        and returns the suffix array as a list '''
+    def depthFirstSearch(self, main_edge = 1, traversed_so_far = 0):
+        suffix_array = list()
+        for edge_value, edge_index in sorted(self.nodes[main_edge].get_edges().iteritems(), key=operator.itemgetter(0)):
+            if self.nodes[edge_index].get_end() == oo:
+                suffix_array.append(self.nodes[edge_index].get_start() - traversed_so_far)
+            else:
+                traversed_so_far += self.nodes[edge_index].get_end() - self.nodes[edge_index].get_start()
+                suffix_array += self.depthFirstSearch(edge_index, traversed_so_far)
+                traversed_so_far -= self.nodes[edge_index].get_end() - self.nodes[edge_index].get_start()
+
+        return suffix_array
+
+    def add_char( self, c ):
+        self.text += c
+        self.pos += 1
+
+        self.need_link = -1
+        self.r += 1
+
+        # while there are suffixes to be added
+        while self.r > 0:
+            if self.active_length == 0:
+                self.active_edge = self.pos
+
+            if self.get_active_edge() not in self.nodes[self.active_node].get_edges():
+                leaf = self.new_node(self.pos, oo)
+                self.nodes[self.active_node].put_edge(self.get_active_edge(), leaf)
+                self.add_suffix_link(self.active_node)
+            else:
+                nex = self.nodes[self.active_node].get_edge(self.get_active_edge())#node that we are going to walk down
+                if self.walk_down(nex):
+                    continue
+                if self.text[self.nodes[nex].get_start() + self.active_length] == c:
+                    self.active_length += 1
+                    self.add_suffix_link(self.active_node)
+                    break
+
+                split = self.new_node(self.nodes[nex].get_start(),
+                                      self.nodes[nex].get_start() + self.active_length)
+                self.nodes[self.active_node].put_edge(self.get_active_edge(), split)
+                leaf = self.new_node(self.pos, oo)
+                self.nodes[split].put_edge(c, leaf)
+                self.nodes[nex].increment_start(self.active_length)
+                self.nodes[split].put_edge(self.text[self.nodes[nex].get_start()], nex)
+                self.add_suffix_link(split)
+
+            self.r -= 1
+            if self.active_node == self.root and self.active_length > 0:
+                self.active_length -= 1
+                self.active_edge = self.pos - self.r + 1
+            else:
+                self.active_node = self.nodes[self.active_node].get_link() if self.nodes[self.active_node].get_link() > 0 else self.root
+
+    def walk_down( self, nex ):
+        if self.active_length >= self.nodes[ nex ].edge_length( self.pos ):
+            self.active_edge += self.nodes[ nex ].edge_length( self.pos )
+            self.active_length -= self.nodes[ nex ].edge_length( self.pos )
+            self.active_node = nex
+            return True
+        return False
+
+
+    def get_active_edge( self ):
+        return self.text[ self.active_edge ]
+
+    def new_node( self, start, end ):
+        self.cur_node += 1
+        self.nodes[ self.cur_node ] = Node( start, end )
+        self.nodes_added += 1
+        return self.cur_node
+
+    def add_suffix_link( self, node ):
+        if self.need_link > 0:
+            self.nodes[ self.need_link ].set_link( node )
+        self.need_link = node
+
+    ''' Modified by agolotin to output the starting and ending indicies of an edge '''
+    def edge_string( self, node ):
+        start = self.nodes[ node ].get_start()
+        end = min( self.pos + 1, self.nodes[ node ].get_end() )
+        _str = self.text[start:end]
+        _str += " " + str(self.nodes[node].get_start()) + ", "
+        _str += "$" if self.nodes[node].get_end() == oo else str(self.nodes[node].get_end())
+        return _str
+
+    def print_tree(self ):
+        self.print_edges( self.root )
+
+    def print_graphviz_tree( self ):
+        print "digraph {"
+        print "\trankdir = LR;"
+        print "\tedge [arrowsize=0.4,fontsize=10]"
+        print "\tnode1 [label=\"\",style=filled,fillcolor=lightgrey,shape=circle,width=.1,height=.1];"
+        print "//------leaves------"
+        self.print_gv_leaves( self.root )
+        print "//------internal nodes------"
+        self.print_gv_internal_nodes( self.root )
+        print "//------edges------"
+        self.print_gv_edges( self.root )
+        #print "//------suffix links------"
+        #self.print_gv_suffix_links( self.root )
+        print "}"
+
+    def print_edges( self, x ):
+        for key, child in self.nodes[ x ].get_edges().iteritems():
+            print self.edge_string( child )
+            self.print_edges( child )
+
+    def print_gv_internal_nodes( self, x ):
+        if x != self.root and len( self.nodes[ x ].get_edges() ) > 0:
+            print "\tnode" + str( x ) + " [label=\"\",style=filled,fillcolor=lightgrey,shape=circle,width=.07,height=.07]"
+        for key, child in self.nodes[ x ].get_edges().iteritems():
+            self.print_gv_internal_nodes( child )
+
+    def print_gv_suffix_links( self, x ):
+        if self.nodes[ x ].get_link() > 0:
+            print "\tnode" + str( x ) + " -> node" + str( self.nodes[ x ].get_link() ) + " [label=\"\",weight=1,style=dotted]"
+        for key, child in self.nodes[ x ].get_edges().iteritems():
+            self.print_gv_suffix_links( child )
+
+    def print_gv_edges( self, x ):
+        for key, child in self.nodes[ x ].get_edges().iteritems():
+            print "\tnode" + str( x ) + " -> node" + str( child ) + " [label=\"" + self.edge_string( child ) + "\",weight=3]"
+            self.print_gv_edges( child )
+
+    def print_gv_leaves( self, x ):
+        if len( self.nodes[ x ].get_edges() ) == 0:
+            print "\tnode" + str( x ) + " [label=\"\",shape=point]"
+        else:
+            for key, child in self.nodes[ x ].get_edges().iteritems():
+                self.print_gv_leaves( child )
+
+if __name__ == "__main__":
+    with open( sys.argv[ 1 ] ) as fh:
+        text = fh.next().strip()
+
+    tree = SuffixTree( len( text ) )
+    for char in text:
+        tree.add_char( char )
+
+    if len( sys.argv ) > 2:
+        if sys.argv[ 2 ] == "gv":
+            tree.print_graphviz_tree()
+    else:
+        tree.print_tree()
